7bad2fd19e09178b366f2de3e67e6f33f0479430
Ariel Garbin <arieldalton97@gmail.com>
2018-11-29 22:26:02 -0200

    alteracoes movieApi

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Movie from "./Movie";
+
+const movies = [
+  {
+    id: 1,
+    title: "Star Wars",
+    desc: "Saving Private Ryan"
+  },
+  {
+    id: 2,
+    title: "Spider Man"
+  },
+  {
+    id: 3,
+    title: "36th Chamber of Shaolin"
+  }
+];
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        {movies.map(movie => (
+          <Movie key={movie.id} movie={movie} desc={movie.desc} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,12 @@
-import React, { Component, Fragment } from 'react';
-import { render } from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import Button from './Button';
+ReactDOM.render(<App />, document.getElementById('root'));
 
-import './style.scss';
-
-class App extends Component {
-  state = {
-    counter: 0,
-  };
-
-  handleClick = () => {
-    this.setState(previousState => ({ counter: previousState.counter + 1 }));
-  };
-
-  render() {
-    return (
-      <Fragment>
-        <h1 className="title">Micro-frontends</h1>
-        <h2>{this.state.counter}</h2>
-        <Button onClick={this.handleClick}>Somar</Button>
-      </Fragment>
-    );
-  }
-}
-
-render(<App />, document.getElementById('app'));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
