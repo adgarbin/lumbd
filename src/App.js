909bdeff8036f9f2ec1114f5139dcb0147d1a84f
Ariel Garbin <arieldalton97@gmail.com>
2018-12-01 12:13:00 -0200

    alteracoes sabado

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import Movie from "./Movie";
-
-const movies = [
-  {
-    id: 1,
-    title: "Star Wars",
-    desc: "Saving Private Ryan"
-  },
-  {
-    id: 2,
-    title: "Spider Man"
-  },
-  {
-    id: 3,
-    title: "36th Chamber of Shaolin"
-  }
-];
+import Movie from './Movie';
 
 class App extends Component {
+  state = {
+    movies: [],
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch(
+        'https://api.themoviedb.org/3/discover/movie?api_key=bae95bd7b609e976bc7603ae04013874&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1',
+      );
+      const movies = await res.json();
+      this.setState({
+        movies: movies.results,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {movies.map(movie => (
-          <Movie key={movie.id} movie={movie} desc={movie.desc} />
+        {this.state.movies.map(movie => (
+          <Movie key={movie.id} movie={movie} />
         ))}
       </div>
     );
